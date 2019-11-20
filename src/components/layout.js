63e2d3a75929ff5dd8c706ac59d0c5772128df4c
sthoyre@gmail.com <samuelthoyre@C02L94W0F6T6L.local>
2019-11-20 13:24:58 -0500

    moved the rest of scss files to styles folder

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Header from "./header"
 import Footer from "./footer"
-import layoutStyles from "./layout.module.scss"
+import layoutStyles from "../styles/layout.module.scss"
 
 const Layout = props => {
 	return (
