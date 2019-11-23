217d2b358ecd6ec2afd61beaae5ca878ddf6a103
sthoyre@gmail.com <samuelthoyre@C02L94W0F6T6L.local>
2019-11-23 14:22:12 -0500

    small changes to navbar and refactored useNavStyles to separate file

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react"
 import { Link, graphql, useStaticQuery } from "gatsby"
 import clsx from "clsx"
-import { makeStyles, useTheme } from "@material-ui/core/styles"
+import { useTheme } from "@material-ui/core/styles"
 import {
   Drawer,
   CssBaseline,
@@ -13,7 +13,6 @@ import {
   IconButton,
   ListItem,
   ListItemIcon,
-  ListItemText,
 } from "@material-ui/core"
 
 import MenuIcon from "@material-ui/icons/Menu"
@@ -23,60 +22,7 @@ import HomeIcon from "@material-ui/icons/Home"
 import CreateIcon from "@material-ui/icons/Create"
 import PhoneIcon from "@material-ui/icons/Phone"
 import ContactSupportIcon from "@material-ui/icons/ContactSupport"
-
-const drawerWidth = 240
-
-const useStyles = makeStyles(theme => ({
-  root: {
-    display: "flex",
-  },
-  link: {
-    color: "white",
-    fontSize: "3rem",
-    textDecoration: "none",
-  },
-  appBar: {
-    transition: theme.transitions.create(["margin", "width"], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-  },
-  appBarShift: {
-    width: `calc(100% - ${drawerWidth}px)`,
-    marginLeft: drawerWidth,
-    transition: theme.transitions.create(["margin", "width"], {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  hide: {
-    display: "none",
-  },
-  drawer: {
-    width: drawerWidth,
-    flexShrink: 0,
-  },
-  drawerPaper: {
-    width: drawerWidth,
-    backgroundColor: "#3f51b5",
-  },
-  drawerHeader: {
-    display: "flex",
-    alignItems: "center",
-    padding: theme.spacing(0, 1),
-    ...theme.mixins.toolbar,
-    justifyContent: "flex-end",
-    backgroundColor: "#3f51b5",
-  },
-
-  icon: {
-    color: "white",
-    fontSize: "2.5rem",
-  },
-}))
+import { useNavStyles } from "../styles/useNavStyles"
 
 export default function Header(props) {
   const data = useStaticQuery(graphql`
@@ -89,7 +35,7 @@ export default function Header(props) {
     }
   `)
   const { title } = data.site.siteMetadata
-  const classes = useStyles()
+  const classes = useNavStyles()
   const theme = useTheme()
   const [open, setOpen] = useState(false)
 
@@ -146,35 +92,35 @@ export default function Header(props) {
         <Divider />
         <List>
           <Link className={classes.link} to="/">
-            <ListItem button>
+            <ListItem style={{ padding: "1rem" }} button>
               <ListItemIcon>
                 <HomeIcon className={classes.icon} />
               </ListItemIcon>
-              <ListItemText primary="Home" />
+              <Typography variant="h6">Home</Typography>
             </ListItem>
           </Link>
           <Link className={classes.link} to="/blog">
-            <ListItem button>
+            <ListItem style={{ padding: "1rem" }} button>
               <ListItemIcon>
                 <CreateIcon className={classes.icon} />
               </ListItemIcon>
-              <ListItemText primary="Blog" />
+              <Typography variant="h6">Blog</Typography>
             </ListItem>
           </Link>
           <Link className={classes.link} to="/about">
-            <ListItem button>
+            <ListItem style={{ padding: "1rem" }} button>
               <ListItemIcon>
                 <ContactSupportIcon className={classes.icon} />
               </ListItemIcon>
-              <ListItemText primary="About" />
+              <Typography variant="h6">About</Typography>
             </ListItem>
           </Link>
           <Link className={classes.link} to="/contact">
-            <ListItem button>
+            <ListItem style={{ padding: "1rem" }} button>
               <ListItemIcon>
                 <PhoneIcon className={classes.icon} />
               </ListItemIcon>
-              <ListItemText primary="Contact" />
+              <Typography variant="h6">Contact</Typography>
             </ListItem>
           </Link>
         </List>
---
+++ src/styles/useNavStyles.js
@@ -0,0 +1,55 @@
+import { makeStyles } from "@material-ui/core/styles"
+
+const drawerWidth = 240
+
+export const useNavStyles = makeStyles(theme => ({
+  root: {
+    display: "flex",
+  },
+  link: {
+    color: "white",
+    fontSize: "3rem",
+    textDecoration: "none",
+  },
+  appBar: {
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  hide: {
+    display: "none",
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+    backgroundColor: "#3f51b5",
+  },
+  drawerHeader: {
+    display: "flex",
+    alignItems: "center",
+    padding: theme.spacing(0, 1),
+    ...theme.mixins.toolbar,
+    justifyContent: "flex-end",
+    backgroundColor: "#3f51b5",
+  },
+
+  icon: {
+    color: "white",
+    fontSize: "2.5rem",
+  },
+}))
